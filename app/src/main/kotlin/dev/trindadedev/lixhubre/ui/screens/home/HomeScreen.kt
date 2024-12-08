b8dd7607bd84e3b0b11cbb857fb5ef512a72fefd
Aquiles Trindade <trindadedev13@gmail.com>
2024-12-07 21:19:10 -0300

    feat: HomeScreen

--- app/src/main/kotlin/dev/trindadedev/lixhubre/ui/screens/home/HomeScreen.kt
+++ app/src/main/kotlin/dev/trindadedev/lixhubre/ui/screens/home/HomeScreen.kt
@@ -6,11 +6,10 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.*
 import androidx.compose.ui.input.nestedscroll.*
-import androidx.compose.ui.layout.*
 import androidx.compose.ui.res.*
-
+import androidx.compose.ui.text.style.TextAlign
 import dev.trindadedev.lixhubre.Strings
-import dev.trindadedev.lixhubre.ui.components.TopBar
+import dev.trindadedev.lixhubre.Drawables
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -21,18 +20,57 @@ fun HomeScreen() {
   Scaffold(
     modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
     topBar = {
-      TopBar(
-        title = { Text(stringResource(id = Strings.app_name)) },
-        scrollBehavior = scrollBehavior,
+      TopAppBar(
+        title = {
+          Text(
+            text = stringResource(id = Strings.app_name),
+            textAlign = TextAlign.Center
+          )
+        },
+        scrollBehavior = scrollBehavior
       )
-    },
+    }
   ) { innerPadding ->
     Column(
       Modifier
         .padding(innerPadding)
+        .fillMaxSize()
         .verticalScroll(scrollState)
     ) {
-      
+      Column(
+        modifier = Modifier
+          .fillMaxSize()
+          .padding(8.dp)
+          .weight(1f),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Bottom
+      ) {
+        Text(
+          text = stringResource(id = Strings.login_message),
+          fontSize = 40.sp,
+          modifier = Modifier.padding(bottom = 50.dp),
+          textAlign = TextAlign.Center,
+        )
+        Button(
+          onClick = {
+          
+          },
+          modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 35.dp),
+          contentPadding = PaddingValues(0.dp)
+        ) {
+          Icon(
+            painter = painterResource(id = Drawables.ic_google),
+            contentDescription = null,
+            modifier = Modifier.padding(end = 8.dp)
+          )
+          Text(
+            text = stringResource(id = Strings.common_word_google),
+            fontSize = 14.sp
+          )
+        }
+      }
     }
   }
 }
